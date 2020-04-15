19110803f01bf779a51e70f14301e4db378e2389
Manuel Ignacio Salas Guzmán <misalasg@gmail.com>
2020-04-15 12:07:42 -0500

    Se modificó app.use

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const PORT = 8080;
 const HOSTNAME = '0.0.0.0';
 
 var app = express();
+/*
 app.use(bodyParser.json());
 app.get('/',(request,response) => {
 	response.send('<h1 style="color: red;">Hola mundo, desde puerto 8080</h1>');
 });
+*/
 app.use(express.static(__dirname + '/prueba'));
 app.get('/prueba',(request,response) => {
 	response.sendFile(path.join(__dirname));
