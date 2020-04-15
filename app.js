e4c9f2b51597b2a96c889d01342e60cac4dc3aa5
Manuel Ignacio Salas Guzmán <misalasg@gmail.com>
2020-04-15 11:51:45 -0500

    Se modificó ruta

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const PORT = 8080;
 const HOSTNAME = '0.0.0.0';
 
@@ -9,8 +10,9 @@ app.use(bodyParser.json());
 app.get('/',(request,response) => {
 	response.send('<h1 style="color: red;">Hola mundo, desde puerto 8080</h1>');
 });
+app.use(express.static(__dirname + '/prueba'));
 app.get('/prueba',(request,response) => {
-	response.sendFile('./prueba');
+	response.sendFile(path.join(__dirname));
 });
 
 app.listen(PORT,HOSTNAME, () => {
